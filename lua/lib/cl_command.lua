ed8bfe4a65f1ab8322eda0331987dbb9a8494b92
Snivo <jrgerba@gmail.com>
2022-05-17 18:48:58 -0400

    fixed returns, removed debug prints, added command

--- lua/lib/cl_command.lua
+++ lua/lib/cl_command.lua
@@ -22,35 +22,32 @@ local errorChatMsg = {
 }
 
 local function printError(error)
-    blusky.debug.print(error)
     chat.AddText(unpack(errorChatMsg[error]))
 end
 
-function command.call(name, ...)
+function command.call(name, args)
     local cmd = command.commandData[name]
 
     if not cmd then
-        printError(command.commandCode.notCommand)
-        return
+        return command.commandCode.notCommand
     end
 
-    local code = cmd.validate(...)
+    local code = cmd.validate(args)
 
     if code ~= command.commandCode.ok then
-        printError(code)
-        return
+        return code
     end
 
-    s_ExecFromClient(cmd, ...)
+    s_ExecFromClient(cmd, args)
+    return command.commandCode.ok
 end
 
 -- Netcode Setup --
 
-
-function s_ExecFromClient(cmd, ...)
+function s_ExecFromClient(cmd, args)
     net.Start("blusky.net.execfromclient")
     net.WriteUInt(cmd.netid, command.net.netidBits)
-    cmd.pack(...)
+    cmd.pack(args)
     net.SendToServer()
 end
 
@@ -59,3 +56,19 @@ function r_CommandError(len, ply)
 end
 
 net.Receive("blusky.net.commanderror", r_CommandError)
+
+-- Command Setup --
+
+local function ExecuteCommand(ply, cmd, args, strargs)
+    args = blusky.util.parseCommand(strargs)
+    local cmd = args[1]
+    table.remove(args, 1)
+
+    local error = command.call(cmd, args)
+
+    if error ~= 0 then
+        printError(error)
+    end
+end
+
+concommand.Add("blusky", ExecuteCommand)
