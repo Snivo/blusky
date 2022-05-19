d7c8a00d3cde900021070a257d7d855f2c574157
Snivo <jrgerba@gmail.com>
2022-05-18 23:57:52 -0400

    More Test Stuff

--- lua/command/test.lua
+++ lua/command/test.lua
@@ -1,23 +1,49 @@
 return {
     name = "test",
 
-    validate = function() 
+    -- Ensures that all arguments are properly accounted for and properly typed
+    parse = function(args) 
+        -- ensure enough arguments exist
+        if #args ~= 2 then
+            return blusky.command.commandCode.badArguments
+        end
+
+        -- arg 1 is a player, convert it as such
+        args[1] = player.find(args[1])
+        if not (IsEntity(args[1]) and args[1]:IsPlayer()) then
+            return blusky.command.commandCode.badArguments
+        end
+
+        -- arg 2 is a number
+        args[2] = tonumber(args[2])
+        if not args[2] then 
+            return blusky.command.commandCode.badArguments
+        end
+
         return blusky.command.commandCode.ok
     end,
 
-    unpack = function()
-        return nil
+    -- Reads the arguments into a table for call (serverside)
+    unpack = function(args)
+        return {
+            net.ReadEntity(),
+            net.ReadInt(net.ReadUInt(6))
+        }
     end,
 
-    pack = function()
-        return nil
+    -- Sets the arguments up for networking (clientside)
+    pack = function(args)
+        net.WriteEntity(args[1])
+        local mlen = blusky.util.getBits(args[2])
+        net.WriteUInt(mlen, 6)
+        net.WriteInt(args[2], mlen)
+
+        return
     end,
 
-    call = function(caller, ...)
-        if not caller then
-            print("Server called me!")
-        else
-            caller:ChatPrint(tostring(caller).." called me!")
-        end
+    call = function(caller, args)
+        local str = "You've been sent the number %s by %s"
+        args[1]:ChatPrint(Format(str, args[2], caller:IsValid() and caller:Name() or "CONSOLE"))
+        return blusky.command.commandCode.ok
     end
 }
