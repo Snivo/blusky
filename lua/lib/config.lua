6b7ea755d868f992617858c0067c959b5c20e502
Snivo <jrgerba@gmail.com>
2022-05-23 23:47:34 -0400

    Added config library

--- lua/lib/config.lua
+++ lua/lib/config.lua
@@ -1,10 +1,11 @@
 local default = {
-
+    commandString = "/"
 }
 
 if !file.Exists("blusky/config.txt", "DATA") then
     file.CreateDir("blusky")
-    file.Create("blusky/config.txt", util.TableToJSON(default))
+    file.Write("blusky/config.txt", util.TableToJSON(default))
 end
 
-blusky.config = util.JSONToTable(file.Read("blusky.config.txt"))
+-- blusky.config = util.JSONToTable(file.Read("blusky.config.txt"))
+blusky.config = default
